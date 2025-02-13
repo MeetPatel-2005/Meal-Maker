527074ab68e84f801941fc5573339ca8402f3f31
MeetPatel-2005 <23dcs085@charusat.edu.in>
2025-02-13 22:28:03 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+require("dotenv").config();
+
+const { app } = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, This is Test Start for \"Meal Maker\"");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
