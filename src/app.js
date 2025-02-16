aa2f5255c3b664c4cfe7de4ca1cf9a1652a5494f
MeetPatel-2005 <23dcs085@charusat.edu.in>
2025-02-16 15:27:30 +0530

    added static files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public/stylesheets")));
+app.use(express.static(path.join(__dirname, "../public/javascripts")));
+app.use(express.static(path.join(__dirname, "../public/images")));
+app.use(express.static(path.join(__dirname, "../public/Gif")));
 
 module.exports = { app };
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const { app } = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, This is Test Start for \"Meal Maker\"");
-})
+    res.render("temp_index.ejs");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
